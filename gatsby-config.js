b304d0674a571caaa7da3a259f7e101fc3ac9f00
Bamigbade, Emmanuel (LNG-LON) <BAMIGBEX@legal.regn.net>
2019-04-01 22:54:42 +0100

    Edit Site title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `4T Knox Secure`,
+    description: `Your one stop shop for all your security needs.`,
     author: `@gatsbyjs`,
   },
   plugins: [
